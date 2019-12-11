8c9286a6eac42048f21ebe5c140c5893c8f19e18
Hiraku Saito <ittun55@gmail.com>
2019-12-11 18:54:28 +0900

    remove unnecessary code

--- src/DokuApi/Controller/BaseController.php
+++ src/DokuApi/Controller/BaseController.php
@@ -1,10 +1,6 @@
 <?php
 namespace Burdock\DokuApi\Controller;
 
-use Burdock\Config\Config;
-use PDO;
-use Psr\Log\LoggerInterface;
-
 class BaseController
 {
     public static function initialize()

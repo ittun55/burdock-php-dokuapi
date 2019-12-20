58dadb7f2977c29514b9944f72e0a71c1c1e1830
Hiraku Saito <ittun55@gmail.com>
2019-12-20 15:48:53 +0900

    return http status code on error

--- src/DokuApi/Dispatcher.php
+++ src/DokuApi/Dispatcher.php
@@ -102,6 +102,7 @@ class Dispatcher
 
     public static function sendErrorResponse($code, $errors): void
     {
+        http_response_code($code);
         header('content-type: application/json; charset=utf-8');
         $err = [ 'code' => $code, 'errors' => $errors ];
         echo json_encode($err, JSON_PRETTY_PRINT);

e312f4bd23d035c3993d3d83e6f537c87c679d63
Hiraku Saito <ittun55@gmail.com>
2019-12-24 18:46:40 +0900

    fixed response json schema

--- src/DokuApi/Response.php
+++ src/DokuApi/Response.php
@@ -10,15 +10,13 @@ class Response
         header('content-type: application/json; charset=utf-8');
         $err = [ 'code' => $code, 'errors' => $errors ];
         echo json_encode($err, JSON_PRETTY_PRINT);
-        exit;
     }
 
     public static function send($code, $data, $setStatus=false): void
     {
         if ($setStatus) http_response_code($code);
         header('content-type: application/json; charset=utf-8');
-        $err = [ 'code' => $code, 'data' => $data ];
-        echo json_encode($err, JSON_PRETTY_PRINT);
-        exit;
+        $data['_code'] = $code;
+        echo json_encode($data, JSON_PRETTY_PRINT);
     }
 }
